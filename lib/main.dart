3813df510a94db4e86eeda5ee46d1dce8cefe5aa
Quentin LEVAVASSEUR <q.levavasseur@live.fr>
2020-11-20 19:02:21 +0100

    clean main.dart & go to respect app design

--- lib/main.dart
+++ lib/main.dart
@@ -29,24 +29,17 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Get the chord you want'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -120,62 +113,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Choose your prefered notation',
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '$chordName',
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                  Text(
+                    '$jsonChord',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                ]
+              )
             ),
-            DropdownButton<String>(
-                items: _notations.map((String value){
-                  return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value)
-                  );
-                }).toList(),
-                value: _currentNotation,
-                onChanged: (String value) {
-                  setState(() {
-                    this._currentNotation = value;
-                    _changeNotation();
-                  });
-                }
+            const Divider(
+              color: Colors.black,
+              height: 20,
+              thickness: 5,
+              indent: 20,
+              endIndent: 0,
             ),
-            Text(
-              'Choose you chord',
-            ),
-            Row(
-              children: <Widget>[
-                DropdownButton<String>(
+            Container(
+              child: Row(
+                children: <Widget>[
+                  DropdownButton<String>(
                     items: _chords.map((String value){
                       return DropdownMenuItem<String>(
                           value: value,
@@ -191,8 +158,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         _incrementCounter();
                       });
                     }
-                ),
-                DropdownButton<String>(
+                  ),
+                  DropdownButton<String>(
                     items: _qualities.map((String value){
                       return DropdownMenuItem<String>(
                           value: value,
@@ -207,8 +174,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         _incrementCounter();
                       });
                     }
-                ),
-                DropdownButton<String>(
+                  ),
+                  DropdownButton<String>(
                     items: _tensions.map((String value){
                       return DropdownMenuItem<String>(
                           value: value,
@@ -223,17 +190,34 @@ class _MyHomePageState extends State<MyHomePage> {
                         _incrementCounter();
                       });
                     }
+                  )
+                ],
+              )
+            ),
+            Container(
+                child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Choose your prefered notation',
+                      ),
+                      DropdownButton<String>(
+                          items: _notations.map((String value){
+                            return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value)
+                            );
+                          }).toList(),
+                          value: _currentNotation,
+                          onChanged: (String value) {
+                            setState(() {
+                              this._currentNotation = value;
+                              _changeNotation();
+                            });
+                          }
+                      )
+                    ]
                 )
-              ],
-            ),
-            Text(
-              '$chordName',
-              style: Theme.of(context).textTheme.headline2,
-            ),
-            Text(
-              '$jsonChord',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
+            )
           ],
         ),
       )// This trailing comma makes auto-formatting nicer for build methods.
