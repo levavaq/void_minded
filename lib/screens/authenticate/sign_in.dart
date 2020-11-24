6f2e3fc8d168d5d0c18b851cc895f1d45f1312b7
Quentin LEVAVASSEUR <q.levavasseur@live.fr>
2020-11-24 11:30:09 +0100

    merge sign-in and register views

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -28,101 +28,132 @@ class _SignInState extends State<SignIn> {
         ? Loading()
         : Scaffold(
             backgroundColor: Color(0xFFFFFFFF),
-            appBar: AppBar(
-              backgroundColor: Color(0xFF222831),
-              elevation: 0.0,
-              title: Text("Sign in to Void Minded"),
-              actions: <Widget>[
-                FlatButton.icon(
-                  icon: Icon(Icons.person),
-                  label: Text("Register"),
-                  onPressed: () {
-                    widget.toggleView();
-                  },
-                )
-              ],
-            ),
             body: Container(
                 padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            SizedBox(height: 20.0),
-                            TextFormField(
-                              decoration: textInputDecoration.copyWith(
-                                  hintText: "Email"),
-                              validator: (emailValue) =>
-                                  emailValue.isEmpty ? "Enter an email" : null,
-                              onChanged: (emailValue) {
-                                setState(() => email = emailValue);
-                              },
-                            ),
-                            SizedBox(height: 20.0),
-                            TextFormField(
-                              decoration: textInputDecoration.copyWith(
-                                  hintText: "Password"),
-                              obscureText: true,
-                              validator: (passwordValue) =>
-                                  passwordValue.length < 6
-                                      ? "Enter a password 6+ chars long"
-                                      : null,
-                              onChanged: (passwordValue) {
-                                setState(() => password = passwordValue);
-                              },
-                            ),
-                            SizedBox(height: 20.0),
-                            RaisedButton(
-                              child: Text("Sign in"),
-                              onPressed: () async {
-                                if (_formKey.currentState.validate()) {
-                                  setState(() => loading = true);
-                                  dynamic result = await _authService
-                                      .signInWithEmailAndPassword(
-                                          email, password);
-                                  if (result == null) {
-                                    setState(() {
-                                      error =
-                                          "Could not sign in with those credentials";
-                                      loading = false;
-                                    });
-                                  }
-                                }
-                              },
-                            ),
-                            SizedBox(height: 12.0),
-                            Text(
-                              error,
-                              style:
-                                  TextStyle(color: Colors.red, fontSize: 14.0),
-                            ),
-                          ],
-                        )),
-                    Image(
-                      width: 100.0,
-                      image: AssetImage(
-                          'lib/assets/images/void_minded_black_logo.png'),
+                    Expanded(
+                      flex: 4,
+                      child: Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SizedBox(height: 20.0),
+                              TextFormField(
+                                decoration: textInputDecoration.copyWith(
+                                    hintText: "Email"),
+                                validator: (emailValue) =>
+                                    emailValue.isEmpty ? "Enter an email" : null,
+                                onChanged: (emailValue) {
+                                  setState(() => email = emailValue);
+                                },
+                              ),
+                              SizedBox(height: 20.0),
+                              TextFormField(
+                                decoration: textInputDecoration.copyWith(
+                                    hintText: "Password"),
+                                obscureText: true,
+                                validator: (passwordValue) =>
+                                    passwordValue.length < 6
+                                        ? "Enter a password 6+ chars long"
+                                        : null,
+                                onChanged: (passwordValue) {
+                                  setState(() => password = passwordValue);
+                                },
+                              ),
+                              SizedBox(height: 20.0),
+                              Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: RaisedButton(
+                                        color: Color(0xFF001818),
+                                        child: Text(
+                                          "Sign in",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        onPressed: () async {
+                                          if (_formKey.currentState.validate()) {
+                                            setState(() => loading = true);
+                                            dynamic result = await _authService
+                                                .signInWithEmailAndPassword(
+                                                    email, password);
+                                            if (result == null) {
+                                              setState(() {
+                                                error =
+                                                    "Could not sign in with those credentials";
+                                                loading = false;
+                                              });
+                                            }
+                                          }
+                                        },
+                                      ),
+                                    ),
+                                    SizedBox(width: 12.0),
+                                    Expanded(
+                                      child: RaisedButton(
+                                        child: Text("Register"),
+                                        onPressed: () async {
+                                          if (_formKey.currentState.validate()) {
+                                            setState(() => loading = true);
+                                            dynamic result = await _authService
+                                                .registerWithEmailAndPassword(
+                                                    email, password);
+                                            if (result == null) {
+                                              setState(() {
+                                                error =
+                                                    "Please supply a valid email";
+                                                loading = false;
+                                              });
+                                            }
+                                          }
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 12.0),
+                              Text(
+                                error,
+                                style:
+                                    TextStyle(color: Colors.red, fontSize: 14.0),
+                              ),
+                            ],
+                          )),
                     ),
-                    RaisedButton(
-                      color: Color(0xFF001818),
-                      child: Text(
-                        "Sign in anonymously",
-                        style: TextStyle(color: Colors.white),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image(
+                            width: 100.0,
+                            image: AssetImage(
+                                'lib/assets/images/void_minded_black_logo.png'),
+                          ),
+                          RaisedButton(
+                            color: Color(0xFF001818),
+                            child: Text(
+                              "Sign in anonymously",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () async {
+                              setState(() => loading = true);
+                              dynamic result =
+                                  await _authService.signInAnonymously();
+                              if (result == null) {
+                                setState(() => loading = true);
+                                print("error signing in");
+                              } else {
+                                print("signed in");
+                                print(result.uid);
+                              }
+                            },
+                          ),
+                        ],
                       ),
-                      onPressed: () async {
-                        setState(() => loading = true);
-                        dynamic result = await _authService.signInAnonymously();
-                        if (result == null) {
-                          setState(() => loading = true);
-                          print("error signing in");
-                        } else {
-                          print("signed in");
-                          print(result.uid);
-                        }
-                      },
                     ),
                   ],
                 )),
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -5,7 +5,6 @@ const textInputDecoration = InputDecoration(
   fillColor: Colors.white,
   filled: true,
   enabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(30.0)),
       borderSide: BorderSide(color: Colors.black87, width: 2.0)),
   focusedBorder: OutlineInputBorder(
       borderSide: BorderSide(color: Colors.blue, width: 2.0)),
